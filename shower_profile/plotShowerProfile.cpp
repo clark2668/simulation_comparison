4dbd094552a8b2dcc1196c10b13b62ac8c6ab33c
clark2668 <baclark@msu.edu>
2020-06-23 14:25:36 -0500

    update it once more to be the had shower case Ben wanted me to check

--- shower_profile/plotShowerProfile.cpp
+++ shower_profile/plotShowerProfile.cpp
@@ -18,6 +18,7 @@
 #include "TGraph.h"
 #include "TCanvas.h"
 #include "Settings.h"
+#include "TAxis.h"
 
 //AraSim includes
 #include "IceModel.h"
@@ -35,8 +36,8 @@ int main(int argc, char **argv)
 	Signal *signal = new Signal(settings);
 	signal->SetMedium(0);
 
-	double shower_E = 1E18;
-	int shower_mode =0; // 0=EM, 1=HAD
+	double shower_E = 6.08986E18;
+	int shower_mode = 1; // 0=EM, 1=HAD
 	double shower_step_m = 0.001; // also settings->SHOWER_STEP
 	int param_model = 0; // 0=jaime's fit, 1= carl's fit 
 
@@ -52,6 +53,7 @@ int main(int argc, char **argv)
 							depth,
 							Q_shower,
 							LQ);
+	cout<<"Q_shower[0] "<<Q_shower[0]<<endl;
 
 	printf("Size of depth is %d \n", depth.size());
 	// // make plot
@@ -59,6 +61,7 @@ int main(int argc, char **argv)
 	TCanvas *c = new TCanvas("","",1100,850);
 	gr->Draw("ALP");
 	gPad->SetLogy();
+	gr->GetYaxis()->SetRangeUser(0.1,3e9);
 	gr->SetTitle("; Depth (m); Shower Profile (some units)");
 	c->SaveAs("profile_vs_depth.png");
 
